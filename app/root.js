153980d507d4f1084f7b26c525a4dbb7c390e32c
pits <tom.pitois@protonmail.com>
2020-06-07 16:58:53 +0200

    new color and listen button

--- app/root.js
+++ app/root.js
@@ -6,9 +6,20 @@ const player = new audio.TNSPlayer();
 const number_to_note = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 const note_to_number = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6};
 
-let note_number;
+let note_number = null;
+let number = null;
 
-exports.generate = function() {
+exports.listen = function() {
+    if (note_number != null) {
+        player.playFromUrl({
+            audioFile: 'https://raw.githubusercontent.com/tpitois/perfectpitch/master/sounds/'+number_to_note[note_number].toLowerCase()+'.mp3',
+            loop: false});
+    }
+}
+
+exports.generate = function(arg) {
+    var lbl = arg.object.parent.parent.getViewById('label');
+    lbl.text = '';
     note_number = Math.floor((Math.random() * 7));
     console.log(number_to_note[note_number]);
     player.playFromUrl({
@@ -17,8 +28,8 @@ exports.generate = function() {
 }
 
 exports.onTap = function(arg) {
-    var lbl = arg.object.parent.parent.getViewById('Label');
-    var number = note_to_number[arg.object.text];
+    var lbl = arg.object.parent.parent.getViewById('label');
+    number = note_to_number[arg.object.text];
     if (number == note_number) {
         lbl.text = 'Perfect ! It was a '+number_to_note[number]+'.';
         lbl.className = 'green';
